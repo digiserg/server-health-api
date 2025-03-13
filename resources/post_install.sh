15466008ba39610b9ba0f0f4d1e00152fe393e78
Sergio Rua <sergio.rua@digitalis.io>
2025-03-13 09:37:39 +0000

    Fixes post install

--- resources/post_install.sh
+++ resources/post_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-useradd -s /bin/false --no-create-home -r health
+getent passwd health > /dev/null || useradd -s /bin/false --no-create-home -r health
 
 [ -x /usr/bin/systemctl ] && systemctl daemon-reload
